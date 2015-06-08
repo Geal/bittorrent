7843934299bc557e9718e58b760e49392e872621
J. Aaron Gustafson <jag426@cornell.edu>
2015-06-07 21:40:39 -0700

    collections::HashMap inside Bencode::Dict

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(collections)]
+#![feature(collections, collections_drain)]
 
 #[macro_use]
 extern crate nom;
@@ -7,17 +7,17 @@ use nom::{IResult, Needed, digit};
 extern crate sha1;
 use sha1::Sha1;
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
-use std::path::Path;
-use std::str;
+use std::path::Path; use std::str;
 
 #[derive(Clone, Debug)]
 enum Bencode {
     Str(Vec<u8>),
     Int(i64),
     List(Vec<Bencode>),
-    Dict(Vec<(Bencode, Bencode)>),
+    Dict(HashMap<Vec<u8>, Bencode>),
 }
 
 named!(i64_utf8<&[u8], i64>, chain!(
@@ -30,7 +30,7 @@ named!(u64_utf8<&[u8], u64>, chain!(
         ||{ str::from_utf8(bytes).unwrap().parse::<u64>().unwrap() }
 ));
 
-fn text(i:&[u8]) -> IResult<&[u8], Bencode> {
+fn text(i:&[u8]) -> IResult<&[u8], Vec<u8>> {
     match u64_utf8(i) {
         IResult::Error(err) => IResult::Error(err),
         IResult::Incomplete(u) => IResult::Incomplete(u),
@@ -41,34 +41,53 @@ fn text(i:&[u8]) -> IResult<&[u8], Bencode> {
             } else {
                 let mut v = vec!();
                 v.push_all(&rest[1..n+1]);
-                IResult::Done(&rest[n+1..], Bencode::Str(v))
+                IResult::Done(&rest[n+1..], v)
             }
         }
     }
 }
 
-named!(int<&[u8], Bencode>, chain!(
+named!(int<&[u8], i64>, chain!(
         tag!("i") ~
      n: i64_utf8  ~
         tag!("e") ,
-        ||{ Bencode::Int(n) }
+        ||{ n }
 ));
 
-named!(list<&[u8], Bencode>, chain!(
+named!(list<&[u8], (Vec<Bencode>)>, chain!(
         tag!("l")       ~
     bs: many0!(bencode) ~
         tag!("e")       ,
-        ||{ Bencode::List(bs) }
+        ||{ bs }
 ));
 
-named!(dict<&[u8], Bencode>, chain!(
-        tag!("d")              ~
-    ps: many0!(pair!(bencode, bencode)) ~
-        tag!("e")              ,
-        ||{ Bencode::Dict(ps) }
+fn dict(i:&[u8]) -> IResult<&[u8], HashMap<Vec<u8>, Bencode>> {
+named!(helper<&[u8], (Vec<(Vec<u8>, Bencode)>)>, chain!(
+        tag!("d")~
+        ps: many0!(pair!(text, bencode))~
+        tag!("e"),
+        ||{ ps }
 ));
+    
+    match helper(i) {
+        IResult::Error(err) => IResult::Error(err),
+        IResult::Incomplete(u) => IResult::Incomplete(u),
+        IResult::Done(rest, mut ps) => {
+            let mut h = HashMap::new();
+            for (k, v) in ps.drain(..) {
+                h.insert(k, v);
+            }
+            IResult::Done(rest, h)
+        }
+    }
+}
 
-named!(bencode<&[u8], Bencode>, alt!(text | int | list | dict));
+named!(bencode<&[u8], Bencode>, alt!(
+        chain!(t:text, ||{Bencode::Str(t)})  |
+        chain!(i: int, ||{Bencode::Int(i)})  |
+        chain!(l:list, ||{Bencode::List(l)}) |
+        chain!(d:dict, ||{Bencode::Dict(d)})
+));
 
 #[derive(Clone, Debug)]
 struct Metainfo {
