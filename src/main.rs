644de33bf9c2f71ec4a840413c506a69f30c42f7
J. Aaron Gustafson <jag426@cornell.edu>
2015-06-10 22:33:25 -0700

    parse bencode as a metainfo, produce metainfo struct

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@
 
 #[macro_use]
 extern crate nom;
-use nom::{IResult, Needed, digit};
+use nom::{IResult, Needed, be_u32, digit};
 
-extern crate sha1;
-use sha1::Sha1;
+//extern crate sha1;
+//use sha1::Sha1;
 
 use std::collections::HashMap;
 use std::fs::File;
@@ -62,12 +62,12 @@ named!(list<&[u8], (Vec<Bencode>)>, chain!(
 ));
 
 fn dict(i:&[u8]) -> IResult<&[u8], HashMap<Vec<u8>, Bencode>> {
-named!(helper<&[u8], (Vec<(Vec<u8>, Bencode)>)>, chain!(
-        tag!("d")~
-        ps: many0!(pair!(text, bencode))~
-        tag!("e"),
-        ||{ ps }
-));
+    named!(helper<&[u8], (Vec<(Vec<u8>, Bencode)>)>, chain!(
+            tag!("d")~
+            ps: many0!(pair!(text, bencode))~
+            tag!("e"),
+            ||{ ps }
+    ));
     
     match helper(i) {
         IResult::Error(err) => IResult::Error(err),
@@ -89,13 +89,112 @@ named!(bencode<&[u8], Bencode>, alt!(
         chain!(d:dict, ||{Bencode::Dict(d)})
 ));
 
+fn s2v(s:&'static str) -> Vec<u8> {
+    let mut v = Vec::new();
+    for &c in s.as_bytes().iter() {
+        v.push(c.clone());
+    }
+    v
+}
+
+#[derive(Clone, Debug)]
+struct Hash {
+    bytes: [u32; 5], // big-endian
+}
+
+named!(hashes<&[u8], (Vec<Hash>)>, many0!(chain!(
+        w0: be_u32~
+        w1: be_u32~
+        w2: be_u32~
+        w3: be_u32~
+        w4: be_u32,
+        ||{Hash {
+            bytes: [w0, w1, w2, w3, w4],
+        }}
+)));
+
 #[derive(Clone, Debug)]
 struct Metainfo {
     announce: String,
     name: String,
-    piecelen: usize,
-    pieces: Vec<[u32; 5]>,
-    files: Vec<(usize, String)>,
+    piecelength: usize,
+    pieces: Vec<Hash>,
+    // exactly one of the two following is None
+    length: Option<usize>,
+    files: Option<Vec<(usize, String)>>,
+}
+
+fn metainfo(i:&[u8]) -> Metainfo {
+    let b = match bencode(i) {
+        IResult::Done(_, b) => b,
+        _ => panic!("Not a bencoding at all"),
+    };
+    let mut d = match b {
+        Bencode::Dict(d) => d,
+        _ => panic!("Not a bencoded dictionary"),
+    };
+    let announce = String::from_utf8(match d.remove(&s2v("announce")).expect("No announce") {
+        Bencode::Str(s) => s,
+        _ => panic!("announce not a string"),
+    }).unwrap();
+    let mut info = match d.remove(&s2v("info")).expect("No info") {
+        Bencode::Dict(d) => d,
+        _ => panic!("info not a dictionary"),
+    };
+    let name = String::from_utf8(match info.remove(&s2v("name")).expect("No name") {
+        Bencode::Str(s) => s,
+        _ => panic!("name not a string"),
+    }).unwrap();
+    let piecelength = match info.remove(&s2v("piece length")).expect("No piece length") {
+        Bencode::Int(i) if i > 0 => i as usize,
+        _ => panic!("piece length not a nonnegative int"),
+    };
+    let piecestr = &match info.remove(&s2v("pieces")).expect("No pieces") {
+        Bencode::Str(s) => s,
+        _ => panic!("pieces not a string"),
+    }[..];
+    if piecestr.len() % 20 != 0 {
+        panic!("pieces not a multiple of 20 bytes long ({})", piecestr.len());
+    }
+    let pieces = match hashes(piecestr) {
+        IResult::Done(_, hs) => hs,
+        _ => panic!("pieces invalid"),
+    };
+    let length = match info.remove(&s2v("length")) {
+        Some(thing) => match thing {
+            Bencode::Int(i) if i >= 0 => Some(i as usize),
+            _ => panic!("length not a nonnegative int")
+        },
+        None => None,
+    };
+    let files = match info.remove(&s2v("files")) {
+        Some(thing) => match thing {
+            Bencode::List(mut l) => Some(l.iter_mut().map(|p| match p {
+                &mut Bencode::Dict(ref mut d) => (
+                    match d.remove(&s2v("length")).expect("No file length") {
+                        Bencode::Int(i) if i >= 0 => i as usize,
+                        _ => panic!("file length not a nonnegative int"),
+                    },
+                    match d.remove(&s2v("path")).expect("No file path") {
+                        Bencode::Str(s) => String::from_utf8(s).unwrap(),
+                        _ => panic!("file path not a string"),
+                    },
+                ),
+                _ => panic!("entry in files not a dictionary"),
+            }).collect()),
+            _ => panic!("files not a list"),
+        },
+        None => None,
+    };
+    assert!(length != None || files != None);
+    Metainfo {
+        announce: announce,
+        name: name,
+        piecelength: piecelength,
+        pieces: pieces,
+        length: length,
+        files: files,
+    }
 }
 
 fn main() {
@@ -103,5 +202,5 @@ fn main() {
     println!("{:?}", f);
     let mut v = Vec::new();
     f.read_to_end(&mut v).ok();
-    println!("{:?}", bencode(&v[..]));
+    println!("{:?}", metainfo(&v[..]));
 }
